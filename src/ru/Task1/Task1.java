099606352dcfc619aed531708613b68986a86afc
DESKTOP-PMQOTRO\Николай <dmitriyrostov2016@gmail.com>
2023-03-09 13:18:30 +0300

    Commit

---
+++ src/ru/Task1/Task1.java
@@ -0,0 +1,36 @@
+package ru.Task1;
+
+import com.sun.source.tree.CatchTree;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        int word = 15; // количество букв в признании
+        int spaces = 3; // количество пробелов в признании
+        int each3word = 62; // количестов сантиметров длины забора на каждые 3 буквы
+        int Lspaces = 12; // количество сантиметров в одном пробеле
+        int Allwords = (15 / 3) * 62; // длина всех букв
+        int Allspaces = 12 * 3; // Длина всех пробелов
+        int confession = Allspaces + Allwords; // Длина признания в сантиметрах
+
+        System.out.println("Введите длину забора в сантиметрах");
+
+        try {
+            Scanner sc = new Scanner(System.in);
+            int L = sc.nextInt();
+            if (L < confession) {
+                System.out.println(" Длины забора недостаточно для надписи в признании любви");
+            } else {
+                System.out.println("Длины забора достаточно для написания признания в любви");
+            }
+            sc.close();
+        }
+        catch (InputMismatchException exception) {
+            System.out.println("Введено не число");
+    }
+}
+}
---
+++ src/ru/Task2/Cactus.java
@@ -0,0 +1,17 @@
+package ru.Task2;
+
+
+public class Cactus {
+
+
+    //констуктор
+    public Cactus() {
+
+    }
+
+    // метод полить каксут
+
+    public void WaterCactuc() {
+        System.out.println("Полить кактус");
+    }
+}
